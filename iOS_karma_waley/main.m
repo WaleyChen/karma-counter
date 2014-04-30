72eb04d399285ecaeca236fe202aeaee910e2f87
Waley Chen <Phoenix@Waleys-MacBook-Air.local>
2014-04-30 16:52:32 -0400

    Initial Commit

---
+++ iOS_karma_waley/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOS_karma_waley
+//
+//  Created by Waley Chen on 2014-04-30.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ iOS_karma_waleyTests/iOS_karma_waleyTests.m
@@ -0,0 +1,34 @@
+//
+//  iOS_karma_waleyTests.m
+//  iOS_karma_waleyTests
+//
+//  Created by Waley Chen on 2014-04-30.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iOS_karma_waleyTests : XCTestCase
+
+@end
+
+@implementation iOS_karma_waleyTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
