92fca383bb72a6fe104c4979c3064c293974c9f1
Waley Chen <Phoenix@Waleys-MacBook-Air.local>
2014-04-30 18:05:54 -0400

    choose nickname view and view controller

---
+++ NicknameViewController.h
@@ -0,0 +1,13 @@
+//
+//  NicknameViewController.h
+//  iOS_karma_waley
+//
+//  Created by Waley Chen on 2014-04-30.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NicknameViewController : UIViewController
+
+@end
